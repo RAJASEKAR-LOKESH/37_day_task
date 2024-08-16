74590c699de7db144f93802540ec836b53336fd4
RAJASEKAR L <84315997+RAJASEKAR-LOKESH@users.noreply.github.com>
2024-08-16 18:02:35 +0530

    Add files via upload

---
+++ routes/fileRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+
+const folderPath="/GUVI/Task/Node JS/37_day/Files"
+router.post('/create', (req, res) => {
+    const moment = require('moment');
+    const timeStamp=moment().format("DD-MM-YYYY-HH-mm-ss")
+    const fileName = `${timeStamp}.txt`;
+    fs.writeFile(`/GUVI/Task/Node JS/37_day/Files/${fileName}`,timeStamp,(err)=>{
+        if(err)
+        {
+            console.log(err)
+            res.send("Error")
+        }
+        else
+        {
+            console.log("File Created Successfully")
+            res.send({message:"File Created Successfully",
+                Filename:fileName
+            })
+        }
+    })
+    
+});
+
+
+router.get('/', (req, res) => {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error reading files' });
+        }
+        const textFiles = files.filter(file => file.endsWith('.txt'));
+        res.status(200).json({ files: textFiles });
+    });
+});
+
+module.exports = router;
